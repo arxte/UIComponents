a7a80baca773d55f7a2bd451c49fdd7cba113acf
Akzhan <ilyasovakzhan@gmail.com>
2023-12-30 22:00:59 +0600

    check

--- src/layout/Default.vue
+++ src/layout/Default.vue
@@ -1,53 +1,58 @@
 <template>
-    <div class="container">
-        <div class="menu">
-            <div v-for="menu in menus" :key="menu.name" class="wrapper-links">
-                <router-link :to="menu.url" class="link">{{ menu.name }}</router-link>
-            </div>
-        </div>
+  <div class="container">
+    <div class="menu">
+      <div v-for="menu in menus" :key="menu.name" class="wrapper-links">
+        <router-link :to="menu.url" class="link">{{ menu.name }}</router-link>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import menus from "@/modules"
-</script>
+import menus from "@/modules";
 
+const check = () => {
+  console.log("checked");
+};
+
+check();
+</script>
 
 <style scoped lang="scss">
 .menu {
-    display: flex;
-    justify-content: space-between;
-    padding: 15px 25px;
-    background-color: $blue-light;
-    border-radius: 6px;
+  display: flex;
+  justify-content: space-between;
+  padding: 15px 25px;
+  background-color: $blue-light;
+  border-radius: 6px;
 }
 
 .link {
-    color: $blue-dark;
-    text-decoration: none;
-    display: block;
+  color: $blue-dark;
+  text-decoration: none;
+  display: block;
 }
 
-.link::first-letter { 
-    text-transform: uppercase;
+.link::first-letter {
+  text-transform: uppercase;
 }
 
 .wrapper-links {
-    position: relative;
+  position: relative;
 }
 
 .wrapper-links:before {
-    content: '';
-    position: absolute;
-    width: 0%;
-    height: 3px;
-    background-color: #fff;
-    bottom: -3px;
-    left: 0;
-    transition: width .2s ease;
+  content: "";
+  position: absolute;
+  width: 0%;
+  height: 3px;
+  background-color: #fff;
+  bottom: -3px;
+  left: 0;
+  transition: width 0.2s ease;
 }
 
 .wrapper-links:hover:before {
-    width: 100%;
+  width: 100%;
 }
 </style>
